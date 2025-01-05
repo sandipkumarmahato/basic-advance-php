bd07bf2a85576a46df4f2e340e06a356556c41d0
sandipkumarmahato <brandifysolution@gmail.com>
2025-01-05 20:25:38 +0530

    Operator

---
+++ 04_operator.php
@@ -0,0 +1,26 @@
+<?php
+    // Arithmetic operators
+
+    $x = 5;
+    $y = 2;
+
+    $sum = $x+$y;
+    $sub = $x-$y;
+    $mult = $x*$y;
+    $div = $x/$y;
+    $modulus = $x%$y;
+    $exponentiation = $x**$y;
+
+
+    echo "$x + $y = $sum";
+    echo "<br>";
+    echo "$x - $y = $sub";
+    echo "<br>";
+    echo "$x * $y = $mult";
+    echo "<br>";
+    echo "$x / $y = $div";
+    echo "<br>";
+    echo "$x % $y = $modulus";
+    echo "<br>";
+    echo "$x ** $y = $exponentiation";
+?>
