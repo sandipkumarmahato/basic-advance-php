a0cee4bfe5e47643c1053521a1ebeecec2def7d1
sandipkumarmahato <brandifysolution@gmail.com>
2025-01-06 00:37:00 +0530

    Grading System

---
+++ 08_Exercise02.php
@@ -0,0 +1,18 @@
+<?php
+    // Grading System
+
+    $marks = 82;
+
+    if ($marks >= 90) {
+        echo "Grade: A";
+    } 
+    elseif ($marks >= 75) {
+        echo "Grade: B";
+    } 
+    elseif ($marks >= 50) {
+        echo "Grade: C";
+    } 
+    else {
+        echo "Grade: F";
+    }
+?>
