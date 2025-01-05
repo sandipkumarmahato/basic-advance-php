e3e514b969032179c979c7cae551509e75effb86
sandipkumarmahato <brandifysolution@gmail.com>
2025-01-05 21:22:03 +0530

    Operator

--- 04_operator.php
+++ 04_operator.php
@@ -51,10 +51,95 @@
     echo $d;
     
     echo "<br>";
-
+    
     $e = 120;  
     $e %= 13;
     echo $e;
-
     
-?>
+    echo "<br>";
+    echo "<br>";
+
+
+    // Comparison Operators
+
+    $x = 100;  
+    $y = "100";
+
+    var_dump($x == $y); // returns true because values are equal
+
+    echo "<br>";
+
+    $x = 100;  
+    $y = "100";
+
+    var_dump($x === $y); // returns false because types are not equal
+
+    echo "<br>";
+
+    $x = 100;  
+    $y = "100";
+
+    var_dump($x != $y); // returns false because values are equal
+
+    echo "<br>";
+    
+    $x = 100;  
+    $y = "100";
+    
+    var_dump($x <> $y); // returns false because values are equal
+
+    echo "<br>";
+    
+    $x = 100;  
+    $y = "100";
+    
+    var_dump($x !== $y); // returns true because types are not equal
+
+    echo "<br>";
+    
+    $x = 100;
+    $y = 50;
+    
+    var_dump($x > $y); // returns true because $x is greater than $y
+    
+    echo "<br>";
+    
+    $x = 10;
+    $y = 50;
+    
+    var_dump($x < $y); // returns true because $x is less than $y
+
+    echo "<br>";
+    
+    $x = 50;
+    $y = 50;
+    
+    var_dump($x >= $y); // returns true because $x is greater than or equal to $y
+    
+    echo "<br>";
+    
+    $x = 50;
+    $y = 50;
+    
+    var_dump($x <= $y); // returns true because $x is less than or equal to $y
+    
+    echo "<br>";
+
+    $x = 5;  
+    $y = 10;
+
+    echo ($x <=> $y); // returns -1 because $x is less than $y
+    echo "<br>";
+
+    $x = 10;  
+    $y = 10;
+
+    echo ($x <=> $y); // returns 0 because values are equal
+    echo "<br>";
+
+    $x = 15;  
+    $y = 10;
+
+    echo ($x <=> $y); // returns +1 because $x is greater than $y
+
+    ?>
