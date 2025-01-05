f187100be1ec4e4edf59b4820f1cd2ffcbd0f7a3
sandipkumarmahato <brandifysolution@gmail.com>
2025-01-05 20:10:14 +0530

    Datatypes

---
+++ 03_dataTypes.php
@@ -0,0 +1,29 @@
+<?php
+    $name = "Sandip Kumar Mahato";
+    var_dump($name);
+
+    echo "<br>";
+
+    $age = 23;
+    var_dump($age);
+
+    echo "<br>";
+
+    $weight = 50.50;
+    var_dump($weight);
+
+    echo "<br>";
+
+    $x = true;
+    var_dump($x);
+
+    echo "<br>";
+
+    $arr = array(1,3,9);
+    var_dump($arr);
+
+    echo "<br>";
+
+    $n = null;
+    var_dump($n);    
+?>
