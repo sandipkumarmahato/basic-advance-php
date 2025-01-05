86c7fe35ac515f143188c9b314392426870e487a
sandipkumarmahato <brandifysolution@gmail.com>
2025-01-06 00:05:09 +0530

    Basic Operator  Questions

---
+++ 05_Exercise01.php
@@ -0,0 +1,37 @@
+<?php
+    echo "Write a script to calculate the sum, difference, product, and quotient of two numbers.";
+    echo "<br>";
+
+    $x = 5;
+    $y = 3;
+
+    $sum = $x+$y;
+    $sub = $x-$y;
+    $mult = $x*$y;
+    $div = $x/$y;
+
+
+    echo "$x + $y = $sum";
+    echo "<br>";
+    echo "$x - $y = $sub";
+    echo "<br>";
+    echo "$x * $y = $mult";
+    echo "<br>";
+    echo "$x / $y = $div";
+    
+    
+    echo "<br><br>";
+    echo "Create a program that takes user input and checks if its even or odd.";
+    echo "<br>";
+
+    $a = 11;
+
+    if($a%2==0){
+        echo "$a is Even No";
+    }
+    else{
+        echo "$a is Odd No";
+    }
+
+
+?>
