d075a99a435f968c3d3baea11fd86de7eb2a7d8f
sandipkumarmahato <brandifysolution@gmail.com>
2025-01-05 20:04:48 +0530

    Variables

---
+++ 02_variables.php
@@ -0,0 +1,12 @@
+<?php
+    $name = "Sandip Kumar Mahato";
+    $age = 23;
+    $weight = 50.50;
+
+
+    echo "Name : ".$name;
+    echo "<br>";
+    echo "Age : ".$age;
+    echo "<br>";
+    echo "Weight : ".$weight;
+?>
